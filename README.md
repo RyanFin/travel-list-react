704b94daecb40dfb05277385b7aa3d18e768817d
RyanFin <ryanfinlayson125@hotmail.com>
2024-03-24 01:14:27 +0000

    items can be added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Travel List
+
+## Managing State
+
+- Deciding how to use state:
+  ![alt text](how-to-use-state.png)
+- [Lifting up state](https://legacy.reactjs.org/docs/lifting-state-up.html)
+  - Move the state (and its setting function) to the closest parent both sibling components have in common and pass the data down using props.
+- State is transferred in a one-way data flow in React.
+- Child to parent communication. This works by passing the state setter function to the child component whilst the state is defined and stored in the parent component. This work around allows the child component to update the parent state. (data flowing up work around).
+  ![alt text](image.png)
--- src/App.js
+++ src/App.js
@@ -1,29 +1,113 @@
-export default function App(){
-return(<div className="app">
-  <Logo />
-  <Form />
-  <PackingList />
-  <Stats />
-</div>);
-}
+import { useState } from "react";
 
-function Logo(){
-  return <h1>🌴 Far Away 🧳</h1>
-}
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 5, packed: true },
+];
 
-function Form(){
-  return( 
-  <div className="add-form">
-  <h3>What do you need for your 😍 trip?</h3>
-  </div>
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    // the array is immutable, so you have to create a new one
+    setItems((items) => [...items, item]); //dependant on previous version of state plus adding a new item to it
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      {/* pass state down (as a function) into the prop to compensate for one-way data flow  */}
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats />
+    </div>
   );
 }
 
-function PackingList(){
- return <div className="list">List</div>
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
 }
 
-function Stats(){
+// populate 'items' state variable with form data and store data in array that will be lifted up from the Form component
+// to the PackingList component, where it will be rendered. Data must be lifted up
+// to the closest common parent component; in this case this is the App component
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (description === "") {
+      return;
+    }
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    // set back to initial state
+    setDescription("");
+    setQuantity(1);
+    console.log(newItem);
+    onAddItems(newItem);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/* <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+         */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      {/* if item is packed, select css to strike through, otherwise nothing */}
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
   return (
     <footer className="stats">
       <em>💼 You have X items on your list, and you already packed X (X%)</em>
